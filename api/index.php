7a43911c174cc5085fc684d22d4dfb2d103d48ea
Michal Skop <michal.skop@kohovolit.eu>
2019-07-27 21:46:14 +0200

    adding 'last'

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 # API proxy for darujme.cz
 
-error_reporting(0);
+error_reporting(E_ALL);
 
 $settings = json_decode(file_get_contents("./settings.json"));
 $url = "https://www.darujme.cz/api/v1/organization/" . $settings->organization . "/pledges-by-filter?apiId=" . $settings->apiId . "&apiSecret=" . $settings->apiSecret . "&projectId=" . $_GET['projectId'];
@@ -16,20 +16,26 @@ foreach($pledges->pledges as $s) {
         }
     }
     if ($support) {
-        // print_r($s);
         $last_transaction = array_slice($s->transactions, -1)[0];
-        $supporters[] = ["given_name" => $s->donor->firstName, "family_name" => $s->donor->lastName, "date" => $last_transaction->receivedAt, "amount" => $last_transaction->outgoingAmount->cents];
+        $new_supporter = ["given_name" => $s->donor->firstName, "family_name" => $s->donor->lastName, "date" => $last_transaction->receivedAt, "amount" => $last_transaction->outgoingAmount->cents, "last" => true];
+        foreach ($supporters as $key => $supporter) {
+            if (($supporter["given_name"] == $new_supporter["given_name"]) and ($supporter["family_name"] == $new_supporter["family_name"])) {
+                $new_supporter["last"] = false;
+                break;
+            }
+        }
+        $supporters[] = $new_supporter;
     }
 }
 
 // API call
 // CORS https://stackoverflow.com/a/25661403/1666623
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET');
-header("Access-Control-Allow-Headers: X-Requested-With");
-header('Cache-Control: no-cache, must-revalidate');
-header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-header('Content-type: application/json; charset=utf-8');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: GET');
+// header("Access-Control-Allow-Headers: X-Requested-With");
+// header('Cache-Control: no-cache, must-revalidate');
+// header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+// header('Content-type: application/json; charset=utf-8');
 echo json_encode($supporters);
 
  ?>
